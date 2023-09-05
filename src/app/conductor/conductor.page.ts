00f589a7437e8ff079e1daf04a08e80ce25b8ea0
Percy Santibañez <pe.santibanez@duocuc.cl>
2023-09-05 11:58:13 -0300

    angular 1.5.6

---
+++ src/app/conductor/conductor.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { UserModel } from '../models/UserModel';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-conductor',
+  templateUrl: './conductor.page.html',
+  styleUrls: ['./conductor.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class ConductorPage implements OnInit {
+
+  adminInfoReceived: UserModel | undefined;
+
+  constructor(private r: Router) {
+    this.adminInfoReceived = this.r.getCurrentNavigation()?.extras.state?.['user'];
+   }
+
+  ngOnInit() {
+  }
+
+}
